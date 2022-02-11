ee2ac291b8536212fe70e68c3a789fb4cd20a1e8
pastelblue <pastelblue0721@gmail.com>
2022-02-11 23:57:59 +0900

    2022-02-11 refactoring

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -4,38 +4,43 @@ const messgeForm = document.querySelector("form#message");
 const nickForm = document.querySelector("form#nick");
 const socket = new WebSocket(`ws://${window.location.host}`);
 
-function makeMessage(type, payload){
-    const msg = {type,payload};
+function makeMessage(type, payload) {
+    const msg = { type, payload };
     return JSON.stringify(msg);
-}
-
-socket.addEventListener("open", () => {
-    console.log("Connected to Server");
-});
-
-socket.addEventListener("message", ( message) => {
-   const li = document.createElement("li");
-   li.innerText = message.data;
-   messgeList.append(li);
-});
-
-socket.addEventListener("close",() => {
-    console.log("Disconnecetd from server");
-});
-
-
-function handleSubmit(event){
+  }
+  
+  function handleOpen() {
+    console.log("Connected to Server ✅");
+  }
+  
+  socket.addEventListener("open", handleOpen);
+  
+  socket.addEventListener("message", (message) => {
+    const li = document.createElement("li");
+    li.innerText = message.data;
+    messageList.append(li);
+  });
+  
+  socket.addEventListener("close", () => {
+    console.log("Disconnected from Server ❌");
+  });
+  
+  function handleSubmit(event) {
     event.preventDefault();
-    const input = messgeForm.querySelector("input");
+    const input = messageForm.querySelector("input");
     socket.send(makeMessage("new_message", input.value));
-    input.value ="";
-}
-
-function handleNickSubmit(event){
+    const li = document.createElement("li");
+    li.innerText = `You: ${input.value}`;
+    messageList.append(li);
+    input.value = "";
+  }
+  
+  function handleNickSubmit(event) {
     event.preventDefault();
     const input = nickForm.querySelector("input");
     socket.send(makeMessage("nickname", input.value));
-}
+    input.value = "";
+  }
 
 
 messgeForm.addEventListener("submit", handleSubmit);
