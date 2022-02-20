80b82649b0da6c873712c55c53fe270957ea36cc
pastelblue <pastelblue0721@gmail.com>
2022-02-20 20:55:57 +0900

    2022-02-20 bulid camera room

--- src/server.js
+++ src/server.js
@@ -12,10 +12,24 @@ app.get("/", (req, res) => res.render("home"));
 app.get("/*", (req, res) => res.redirect("/"));
 
 
-const handleListen = () => console.log(`Listening on http://localhost:3000`);
-
 const httpServer = http.createServer(app);
 const wsServer = new Server(httpServer);
 
-
-httpServer.listen(3000, handleListen);
+wsServer.on("connection", (socket) => {
+    socket.on("join_room", (roomName) => {
+      socket.join(roomName);
+      socket.to(roomName).emit("welcome");
+    });
+    socket.on("offer", (offer, roomName) => {
+      socket.to(roomName).emit("offer", offer);
+    });
+    socket.on("answer", (answer, roomName) => {
+      socket.to(roomName).emit("answer", answer);
+    });
+    socket.on("ice", (ice, roomName) => {
+      socket.to(roomName).emit("ice", ice);
+    });
+  });
+  
+  const handleListen = () => console.log(`Listening on http://localhost:3000`);
+  httpServer.listen(3000, handleListen);
